7bcbbd2e20b71bc9f54c9cacd4f9b11306f6494f
jayarajpmca <jayarajpmca@gmail.com>
2016-09-27 19:20:11 +0530

    button clicked

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,8 @@
-alert("Loadeed");
-
 var button=document.getElementById('Submit');
 var counter=0;
 
 button.onclick= function(){
-    
+    alert("Loadeed");
     counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML = counter.toString();
